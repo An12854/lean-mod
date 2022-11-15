3baa4c38976bc4b514b32ef9f0673dd8c4629a08
an12854 <an12854@sapo.pt>
2022-11-15 15:46:26 +0000

    Update Files

--- src/main/java/com/an12854/leanmod/block/ModBlocks.java
+++ src/main/java/com/an12854/leanmod/block/ModBlocks.java
@@ -18,11 +18,25 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
+
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Leanmod.MODID);
     public static final RegistryObject<LiquidBlock> LEAN_WATER_BLOCK = BLOCKS.register("lean_water_block",
             () -> new LiquidBlock(ModFluids.SOURCE_LEAN_WATER, BlockBehaviour.Properties.copy(Blocks.WATER)));
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                            CreativeModeTab tab) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(tab)));
+    }
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
     }
